ff217f9d6a310995468e38e98c907d596088a900
vinit <vinitk81144@gmail.com>
2026-06-22 14:09:58 +0530

    implemented fonts

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,34 +1,34 @@
 import { styled } from "nativewind";
 import { Link } from "expo-router";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link
-        href={"/OnBoarding"}
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Go to OnBoarding
-      </Link>
-      <Link
-        href={"/(auth)/sign-in"}
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Sign In
-      </Link>
-      <Link
-        href={"/(auth)/sign-up"}
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Sign Up
-      </Link>
+    <SafeAreaView className="flex-1 bg-background">
+      <View className="p-5">
+        <Text className="text-5xl font-extrabold">Home</Text>
+        <Link
+          href={"/OnBoarding"}
+          className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+        >
+          Go to OnBoarding
+        </Link>
+        <Link
+          href={"/(auth)/sign-in"}
+          className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+        >
+          Sign In
+        </Link>
+        <Link
+          href={"/(auth)/sign-up"}
+          className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+        >
+          Sign Up
+        </Link>
+      </View>
     </SafeAreaView>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,24 @@
 import "../global.css";
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("../../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-bold": require("../../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-medium": require("../../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-light": require("../../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "sans-extra-bold": require("../../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
