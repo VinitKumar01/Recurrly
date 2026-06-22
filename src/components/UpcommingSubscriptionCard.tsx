7eb3012c06834b5db7430fa13e76b5bb8a506c87
vinit <vinitk81144@gmail.com>
2026-06-23 00:14:55 +0530

    added balance card and upcomming subscriptions card to homepage

---
+++ src/components/UpcommingSubscriptionCard.tsx
@@ -0,0 +1,32 @@
+import { formatCurrency } from "@/lib/utils";
+import { Image as ExpoImage } from "expo-image";
+import { styled } from "nativewind";
+import { View, Text } from "react-native";
+
+const Image = styled(ExpoImage);
+
+export default function UpcommingSubscriptionCard({
+  name,
+  price,
+  daysLeft,
+  icon,
+  currency,
+}: UpcomingSubscriptionCardProps) {
+  return (
+    <View className="upcoming-card shadow-gray-500 shadow-sm drop-shadow-sm my-2 mx-1">
+      <View className="upcoming-row">
+        <Image source={icon} className="upcoming-icon" />
+        <View>
+          <Text className="upcoming-price">
+            {formatCurrency(price, currency)}
+          </Text>
+          <Text className="upcoming-meta">
+            {daysLeft > 1 ? `${daysLeft} days left` : "Last day"}
+          </Text>
+        </View>
+      </View>
+
+      <Text className="upcoming-name">{name}</Text>
+    </View>
+  );
+}
